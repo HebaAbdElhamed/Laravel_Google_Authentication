6063a7d0a7b9f839467459b017e5537ec127a769
Heba Abd Elhamed <hebaabdelhamede@gmail.com>
2024-10-06 14:40:16 +0300

    public

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,6 @@
+<a href="https://hebaabdelhamed.netlify.app/" >
+    <p style="font-weight: 900;font-size:2.5rem;font-family: 'Comic Sans MS', cursive;text-align:center;" >Heba Auth</p>
+    <a href="https://github.com/HebaAbdElhamed">
+        <p style="font-weight: 900;font-family: 'Comic Sans MS', cursive;text-align:center;">Githup Link !</p>
+    </a>
+</a>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
+
+
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::controller(GoogleController::class)->group(function(){
+    Route::get('auth/google', 'redirectToGoogle')->name('auth.google');
+    Route::get('auth/google/callback', 'handleGoogleCallback');
+});
+
